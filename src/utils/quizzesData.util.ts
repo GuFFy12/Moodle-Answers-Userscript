209a84c6360c869b71f62b5ef8e2b335230f0585
GuFFy12 <golovin.mv.vl5@gmail.com>
2023-07-29 03:11:27 +0500

    Dumb shit fix

--- src/utils/quizzesData.util.ts
+++ src/utils/quizzesData.util.ts
@@ -1,99 +1,95 @@
 import { QuizData } from '../types';
 
-function quizzesDataUtil() {
-	class QuizzesDataUtil implements Map<string, QuizData | undefined> {
-		private map = new Map<string, QuizData | undefined>();
+class QuizzesDataUtil implements Map<string, QuizData | undefined> {
+	private map = new Map<string, QuizData | undefined>();
 
-		private update() {
-			const quizzesData = GM_getValue<string | undefined>('quizData', undefined);
+	private update() {
+		const quizzesData = GM_getValue<string | undefined>('quizData', undefined);
 
-			if (quizzesData) {
-				try {
-					this.map = new Map(Object.entries(JSON.parse(quizzesData) as Record<string, QuizData | undefined>));
-					return;
-				} catch (error) {
-					console.log('Ошибка с локальными ответами на тест =>', error);
-				}
-			} else {
-				console.log('Локальные ответы на тест не были найдены');
+		if (quizzesData) {
+			try {
+				this.map = new Map(Object.entries(JSON.parse(quizzesData) as Record<string, QuizData | undefined>));
+				return;
+			} catch (error) {
+				console.log('Ошибка с локальными ответами на тест =>', error);
 			}
-
-			this.save();
+		} else {
+			console.log('Локальные ответы на тест не были найдены');
 		}
 
-		private save() {
-			GM_setValue('quizData', JSON.stringify(Object.fromEntries(this.map)));
-		}
-
-		get [Symbol.toStringTag]() {
-			this.update();
-			return this.map[Symbol.toStringTag];
-		}
-		get size() {
-			this.update();
-			return this.map.size;
-		}
-
-		[Symbol.iterator]() {
-			this.update();
-			return this.map.entries();
-		}
-
-		clear() {
-			this.update();
-			this.map.clear();
-			this.save();
-		}
-
-		delete(key: string) {
-			this.update();
-			const mapDelete = this.map.delete(key);
-			this.save();
-			return mapDelete;
-		}
-
-		entries() {
-			this.update();
-			return this.map.entries();
-		}
-
-		forEach(
-			callbackfn: (value: QuizData | undefined, key: string, map: Map<string, QuizData | undefined>) => void,
-			thisArg?: unknown,
-		) {
-			this.update();
-			return this.map.forEach(callbackfn, thisArg);
-		}
-
-		get(key: string) {
-			this.update();
-			return this.map.get(key);
-		}
-
-		has(key: string) {
-			this.update();
-			return this.map.has(key);
-		}
-
-		keys() {
-			this.update();
-			return this.map.keys();
-		}
-
-		set(key: string, value: QuizData | undefined) {
-			this.update();
-			this.map.set(key, value);
-			this.save();
-			return this;
-		}
-
-		values() {
-			this.update();
-			return this.map.values();
-		}
+		this.save();
 	}
 
-	return new QuizzesDataUtil();
+	private save() {
+		GM_setValue('quizData', JSON.stringify(Object.fromEntries(this.map)));
+	}
+
+	get [Symbol.toStringTag]() {
+		this.update();
+		return this.map[Symbol.toStringTag];
+	}
+	get size() {
+		this.update();
+		return this.map.size;
+	}
+
+	[Symbol.iterator]() {
+		this.update();
+		return this.map.entries();
+	}
+
+	clear() {
+		this.update();
+		this.map.clear();
+		this.save();
+	}
+
+	delete(key: string) {
+		this.update();
+		const mapDelete = this.map.delete(key);
+		this.save();
+		return mapDelete;
+	}
+
+	entries() {
+		this.update();
+		return this.map.entries();
+	}
+
+	forEach(
+		callbackfn: (value: QuizData | undefined, key: string, map: Map<string, QuizData | undefined>) => void,
+		thisArg?: unknown,
+	) {
+		this.update();
+		return this.map.forEach(callbackfn, thisArg);
+	}
+
+	get(key: string) {
+		this.update();
+		return this.map.get(key);
+	}
+
+	has(key: string) {
+		this.update();
+		return this.map.has(key);
+	}
+
+	keys() {
+		this.update();
+		return this.map.keys();
+	}
+
+	set(key: string, value: QuizData | undefined) {
+		this.update();
+		this.map.set(key, value);
+		this.save();
+		return this;
+	}
+
+	values() {
+		this.update();
+		return this.map.values();
+	}
 }
 
-export default quizzesDataUtil();
+export default new QuizzesDataUtil();
